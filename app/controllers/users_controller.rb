551c7ffa629bb7a30671cac29cc626e5e77ba714
betsy_manuel <betsy.manuel03@gmail.com>
2021-01-31 22:53:15 -0800

    modified user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
+    before_action :authenticate_user!, only: [:edit, :show]
   
     def new
         @user=User.new
